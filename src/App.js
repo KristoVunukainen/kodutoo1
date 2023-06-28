d02d23c525490f58e1350b4f84f35e69607ad247
KristoVunukainen <81011724+KristoVunukainen@users.noreply.github.com>
2023-06-28 20:06:16 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className='nupp'><p className='nupu-tekst'>Nupp</p></button>
+      <p className='sinine-text'> <i>See on sinine tekst</i></p>
+      <p className='roheline-text'> See on roheline tekst</p>
+
+      <div className='pildi-kast'><img className='pilt' src="https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg" alt="" />
+      </div>
     </div>
   );
 }
